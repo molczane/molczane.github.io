7243bee5498c148d0c23e826c65da399914d7dfd
molczane <ernest.molczan@gmail.com>
2024-12-19 11:27:33 +0100

    Trying to make things work

--- composeApp/src/wasmJsMain/kotlin/org/dotnet/app/view/DropdownMenu.kt
+++ composeApp/src/wasmJsMain/kotlin/org/dotnet/app/view/DropdownMenu.kt
@@ -5,9 +5,7 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowDropDown
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 
 @Composable
